3dbfc30fb586f8a584cec756d0b69035812f5f90
Johndoe <56298669+togane@users.noreply.github.com>
2022-06-28 20:59:09 +0900

    add Tile component

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,6 @@
 // このアプリでは数牌しか扱わない
 // 字牌も考慮すればもっと汎用的な型定義があると思うが、上記理由からすべての牌は色と数を持つ
-export const TILE_COLORS = ['man', 'pin', 'sou'] as const;
+export const TILE_COLORS = ['m', 'p', 's'] as const;
 export type TileColor = typeof TILE_COLORS[number];
 
 export const ALL_TILE_NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9] as const;
