227df0845b4f726935789a29865609f5b1ac7f4e
Johndoe <56298669+togane@users.noreply.github.com>
2022-07-04 20:58:41 +0900

    show an animation when correct answer submitted

--- src/features/game/components/Game.tsx
+++ src/features/game/components/Game.tsx
@@ -5,12 +5,25 @@ import { Board } from './Board';
 import { Hand } from './Hand';
 import { Question } from './Question';
 import { chooseQuestions } from '@/utils/question';
+import { CorrectAnswerAnimation } from '@/components/CorrectAnswerAnimation';
 
 // TODO: implement this.
 const [hand, machi] = chooseQuestions(2)[0];
 
+const delay = (ms: number) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
+
 export const Game: React.FC = () => {
-  const [Modal, open, close, isOpen] = useModal('root', {
+  const [QuestionModal, openQuestionModal, closeQuestionModal] = useModal(
+    'root',
+    {
+      preventScroll: true,
+      closeOnOverlayClick: false,
+    }
+  );
+
+  const [CheckModal, openCheckModal, closeCheckModal] = useModal('root', {
     preventScroll: true,
     closeOnOverlayClick: false,
   });
@@ -35,13 +48,19 @@ export const Game: React.FC = () => {
 
   const answer = (numbers: number[]): void => {
     if (parseInt(numbers.sort().join('')) === machi) {
-      close();
+      closeQuestionModal();
+      (async () => {
+        // TODO: should quit this workaround.
+        openCheckModal();
+        await delay(2000);
+        closeCheckModal();
+      })();
     }
   };
 
   return (
     <Styled>
-      <div className='board-container' onClick={open}>
+      <div className='board-container' onClick={openQuestionModal}>
         <Board>
           <div className='hand'>
             <Hand number={hand} />
@@ -50,9 +69,13 @@ export const Game: React.FC = () => {
       </div>
 
       <div className='modal-container'>
-        <Modal>
+        <QuestionModal>
           <Question answer={answer} />
-        </Modal>
+        </QuestionModal>
+
+        <CheckModal>
+          <CorrectAnswerAnimation />
+        </CheckModal>
       </div>
     </Styled>
   );
